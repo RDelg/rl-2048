7c547188d3b2bffc8515929b6d78005cad904ce1
Rene Delgado <rene854@gmail.com>
2020-04-27 02:10:05 +0000

    Fixes

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 root_dir='test01'
 num_iterations=1000000
-gin_file=./config,gin
+gin_file=./config.gin
 
-python tf_agents/agents/dqn/examples/v2/train_eval.py \
-  --root_dir=$HOME/tmp/dqn_rnn/gym/MaskedCartPole-v0/ \
+python src/model.py \
+  --root_dir=${root_dir} \
   --num_iterations=${num_iterations} \
   --gin_file=${gin_file}
